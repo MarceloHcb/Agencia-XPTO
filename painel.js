118907e03d343642433db8c0fe299f1d4d530b28
Your Name <you@example.com>
2022-01-17 22:25:04 -0300

    landing page

---
+++ painel.js
@@ -0,0 +1,36 @@
+const imagensPainel = document.querySelectorAll('.imagem-painel');
+const setaAvancar = document.getElementById('btn-avancar');
+const setaVoltar = document.getElementById('btn-voltar');
+
+let imagemAtual = 0;
+function esconderImagem() {
+    imagensPainel.forEach(img => {
+        img.classList.remove("mostrar")
+    })
+}
+function mostrarImagem() {
+    imagensPainel[imagemAtual].classList.add("mostrar")
+}
+setaAvancar.addEventListener("click", function () {
+    const totalDeImagens = imagensPainel.length - 1;
+    if (imagemAtual === totalDeImagens) {
+        return;
+        console.log(imagemAtual)
+        console.log(imagensPainel)
+    }
+    imagemAtual++;
+
+    esconderImagem()
+    mostrarImagem()
+
+})
+
+setaVoltar.addEventListener("click", function () {
+    if (imagemAtual === 0) {
+        return;
+    }
+    imagemAtual--;
+
+    esconderImagem()
+    mostrarImagem()
+})
